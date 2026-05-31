530cdc174d8ff2d1fee036503f144638fb15d19b
Cui, Fawei <fawei.cui@philips.com>
2026-05-31 12:38:28 +0800

    remove pipeline doc

--- earth-pipeline/Build/Actions/Uninstall/Uninstall_CT_Serviceability.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM Post Uninstall Script
-
-
--- earth-pipeline/Build/CI/CT_CICD/CommonPropertiesAssemblyInfo/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("Philips Healthcare Nederland B.V.”)]
-[assembly: AssemblyProduct("Philips CT")]
-[assembly: AssemblyCopyright("Copyright 2024 Koninklijke Philips N.V.")]
-[assembly: AssemblyTrademark("Koninklijke Philips N.V.")]
